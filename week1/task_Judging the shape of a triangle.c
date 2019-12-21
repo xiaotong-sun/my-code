e0e31a77c9f653e6bcf2c9b0956b3ccc7af59ad7
xiaotong-sun <58971249+xiaotong-sun@users.noreply.github.com>
2019-12-21 10:08:38 +0800

    Update task_Judging the shape of a triangle.c

--- week1/task_Judging the shape of a triangle.c
+++ week1/task_Judging the shape of a triangle.c
@@ -10,23 +10,23 @@ int main()
 		float a, b, c;
 		printf("请输入三角形的三边长：");
 		scanf("%f%f%f", &a, &b, &c);
-		if (a + b > c && a + c > b && b + c > a)
-			{
-				if(fabs(a * a + b * b - c * c) <= EPS
-				 || fabs(a * a + c * c - b * b) <= EPS
-				 || fabs(b * b + c * c - a * a) <= EPS)
-					printf("三角形为直角三角形\n");
-				else if (fabs(a - b) <= EPS
-				       || fabs(a - c) <= EPS
-				       || fabs(b - c) <= EPS)
-					printf("三角形为等腰三角形\n");
-				else 
-					printf("三角形为一般三角形\n");
+		if (a + b > c && a + c > b && b + c > a) {
+			if(fabs(a * a + b * b - c * c) <= EPS
+			|| fabs(a * a + c * c - b * b) <= EPS
+			|| fabs(b * b + c * c - a * a) <= EPS) {
+				printf("三角形为直角三角形\n");
 			}
-		else 
+			else if (fabs(a - b) <= EPS
+		      	      || fabs(a - c) <= EPS
+		              || fabs(b - c) <= EPS) {
+				printf("三角形为等腰三角形\n");
+			} else {
+				printf("三角形为一般三角形\n");
+			}
+		} else {
 			printf("不构成三角形\n"); 
-		getch();
+		}
+	getch();
 	}
-	
 	return 0; 
 }
