7441f1be523b999725609692cbbbc8362486ce2d
xiaotong-sun <58971249+xiaotong-sun@users.noreply.github.com>
2019-12-21 09:59:10 +0800

    Update task_the system of the students' scores management..cpp

--- week1/task_the system of the students' scores management..cpp
+++ week1/task_the system of the students' scores management..cpp
@@ -1,25 +1,23 @@
 #include <stdio.h>
  
 int get_failed_student_count(float score[], int n);
-void put_score_in_order(float score[], int n);
+void put_score_in_descending_order(float score[], int n);
 void exchange_score(float score[], int k, int i);
 
 int main()
 {
 	float score[40];
-	int n, i, a;
+	int n, i, total;
 	printf("please input the number of the students(n<=40):");
 	scanf("%d",&n);
 	printf("please input the score of the students:\n");
-	for (i=0; i<n; i++)
-	{
+	for (i = 0; i < n; i++) {
 		scanf("%f",&score[i]);
 	}
-	a = get_failed_student_count(score, n);
-	printf("不及格的人数为：%d\n", a);
-	put_score_in_order(score, n);
-	for (i=0; i<n; i++)
-	{
+	total = get_failed_student_count(score, n);
+	printf("不及格的人数为：%d\n", total);
+	put_score_in_descending_order(score, n);
+	for (i = 0; i < n; i++) {
 		printf("%.1f\n", score[i]);
 	}
 	return 0;
@@ -27,32 +25,26 @@ int main()
 
 int get_failed_student_count(float score[], int n)
 {
-	int i, failed_student_count=0;
-	for (i=0; i<n; i++)
-	{
-		if (score[i] < 60)
-		{
+	int i, failed_student_count = 0;
+	for (i = 0; i < n; i++) {
+		if (score[i] < 60) {
 			failed_student_count += 1;
 		}
 	}
 	return failed_student_count;
 }
 
-void put_score_in_order(float score[], int n)
+void put_score_in_descending_order(float score[], int n)
 {
 	int k;
-	for (int i=0; i<n-1; i++)
-	{
-		k = i;
-		for (int j=i+1; j<n; j++)
-		{
-			if (score[k] < score[j])
-			{
+	for (int i = 0; i < n-1; i++) {
+		k = i; 
+		for (int j = i+1; j < n; j++) {
+			if (score[k] < score[j]) {
 				k = j;
 			}
 		}
-		if (k != i)
-		{
+		if (k != i) {
 			exchange_score(score, k, i);
 		}
 	}
