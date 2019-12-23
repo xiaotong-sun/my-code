e65358a608e3e4edeb1c7e6f6aae1a588f212954
xiaotong-sun <58971249+xiaotong-sun@users.noreply.github.com>
2019-12-23 21:14:58 +0800

    Update exercise P1008 in LuoGu.cpp

--- week2/exercise P1008 in LuoGu.cpp
+++ week2/exercise P1008 in LuoGu.cpp
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
+int isvalid(int Double, int Triple, int Flag);
+
 int main()
 {
-	int num1, num2, num3;
-	for(int i = 1; i < 10; i++) {
-		for(int j = 1; j < 10; j++) {
-			if(i == j) {
+	int Num, Double, Triple, Flag=0;
+	for (int i = 1; i < 10; i++) {
+		for (int j = 1; j < 10; j++) {
+			if (i == j) {
 				continue;
 			} else {
-				for(int k = 1; k < 10; k++) {
-					if(i == k || j == k) {
+				for (int k = 1; k < 10; k++) {
+					if (i == k || j == k) {
 						continue;
 					} else {
-						num1 = i * 100 + j * 10 + k; // Get a three-digit number that is different for each digit.
-						num2 = num1 * 2;
-						num3 = num1 * 3;             // Get three numbers in the ratio of 1:2:3
-						if(num2 % 10 == num2 / 10 % 10 
-						|| num2 / 100 == num2 / 10 % 10 
-						|| num2 % 10 ==  num2 / 100
-						|| num3 % 10 == num3 / 10 % 10 
-						|| num3 / 100 == num3 / 10 % 10 
-						|| num3 % 10 ==  num3 / 100
-						|| num3 >= 1000) {
+						Num = i * 100 + j * 10 + k; // Get a three-digit number that is different for each digit.
+						Double = Num * 2;
+						Triple = Num * 3;             // Get three numbers in the ratio of 1:2:3
+						Flag=isvalid(Double, Triple, Flag);
+						if (Flag == 0) {
 							continue;
 						} else {
-                                                        // Output everything in ascending order.
-							printf("%d %d %d\n", num1, num2, num3);
+							// Output everything in ascending order.
+							printf("%d %d %d\n", Num, Double, Triple);
 						}
 					}
 				}
@@ -33,4 +30,19 @@ int main()
 		}
 	}
 	return 0;
+ } 
+ 
+ int isvalid(int Double, int Triple, int Flag)
+ {
+ 	if (Double % 10 == Double / 10 % 10 
+		|| Double / 100 == Double / 10 % 10 
+		|| Double % 10 ==  Double / 100
+		|| Triple % 10 == Triple / 10 % 10 
+		|| Triple / 100 == Triple / 10 % 10 
+		|| Triple % 10 ==  Triple / 100
+		|| Triple >= 1000) {
+		return 0;
+	} else {
+		return 1;
+	}
  }
