4358802bcf68c95d33d002bd01dc451cb10e00e4
xiaotong-sun <840977419@qq.com>
2020-02-11 19:30:20 +0800

    fix but is not perfect P1036

--- week2/luogu_P1036.cpp
+++ week2/luogu_P1036.cpp
@@ -4,6 +4,7 @@
 #define A 10000
 
 int Is_prime(long sum);
+void deal_with_data(long sum[A], long x[20]);
 
 int main() {
     int n, k, t = -1, total = 0;
@@ -13,14 +14,7 @@ int main() {
     for(int i = 0; i < n; i ++) {
         scanf("%ld", &x[i]);
     }
-    for(int i = 0; i < k - 2; i ++) {
-        for(int j = i + 1; j < k - 1; k ++ ) {
-            for(int m = j + 1; m < k; m ++ ) {
-                t ++;
-                sum[t] = x[i] + x[j] + x[m];
-            }
-        }
-    }
+    deal_with_data(sum, x);
     for(int i = 0; i < t; i ++) {
         if(Is_prime(sum[i])) {
             total ++;
@@ -28,6 +22,10 @@ int main() {
     }
     printf("%d", total);
     return 0;
+}
+
+void deal_with_data(long sum[A], long x[20]) {
+	
 }
 
 int Is_prime(long sum) {
