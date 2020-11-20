63d9b969cd8921536835ddc9f0b477c19cde6285
xiaotong-sun <840977419@qq.com>
2020-11-20 22:41:49 +0800

    modified Fibonacci.c

--- Data_Structures_and_Algorithms/Fibonacci_Search.c
+++ Data_Structures_and_Algorithms/Fibonacci_Search.c
@@ -1,3 +1,5 @@
+// Fibonacci_Search.c
+
 #include <stdio.h>
 
 #define MAX_SIZE 20
@@ -17,20 +19,20 @@ int Fibonacci_Search(int *a, int n, int key) {     //a为需要查找的数组，n为a的长
     int F[MAX_SIZE];
     Fibonacci(F);
     int u = 0;
-    while (n > F[u]) {
+    while (n > F[u]) {						//计算n位于斐波那契数列的位置
         u ++;
     }
-    for (int i = n; i < F[u] - 1; ++i) {
+    for (int i = n; i < F[u] - 1; ++i) {	//如果n < F[u] - 1,则用数组最后一个数补位，直到n = F[u] -1
         a[i + 1] = a[i];
     }
     int low = 1;
     int high = n;
     while (low <= high) {
         int mid = low + F[u - 1] - 1;
-        if (mid >= n && a[n] == key) {
+        if (mid > n && a[n] == key) {		//若mid>=n则说明是扩展的数值,返回n
             return n;
-        } else if (mid >= n){
-            return -2;
+        } else if (mid > n){
+            return -2;						//查找的数据不存在
         }
         if (key == a[mid]) {
             return mid;
@@ -42,7 +44,7 @@ int Fibonacci_Search(int *a, int n, int key) {     //a为需要查找的数组，n为a的长
             u--;
         }
     }
-    return -2; //数据不存在
+    return -2; 								//查找的数据不存在
 }
 
 int main() {
